e3a205832eee407e74b7bd2fd7510bc30710c7a3
Siarhei Yunakh <siarhei_yunakh@epam.com>
2015-04-14 18:24:46 +0300

    added interface ChangedRecord

--- src/Mvvm.TypeScript/libs/object.observe.d.ts
+++ src/Mvvm.TypeScript/libs/object.observe.d.ts
@@ -1,6 +1,13 @@
 ﻿interface ObjectConstructor {
-    observe(object: any, handler: (update: any) => any, acceptList?: Array<string>): any;
-    unobserve(beingObserved: any, handler: (update: any) => any): any;
+    observe(object: any, handler: (update: Array<ChangedRecord>) => any, acceptList?: Array<string>): any;
+    unobserve(beingObserved: any, handler: (update: Array<ChangedRecord>) => any): any;
     getNotifier(object: any): any;
     deliverChangeRecords(handler: (update: any) => any): any;
+}
+
+interface ChangedRecord {
+    "type": string;
+    oldValue: any;
+    name: string;
+    object: any;
 }
